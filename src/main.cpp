0705928fe230eae74cc48a72f13ae36c1f293aee
mdco-ld <migueldco14@gmail.com>
2025-02-04 20:00:54 -0300

    make the program use the alternative screen

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,30 @@
 #include <iostream>
 
 int main() {
-	if (!isatty(STDOUT_FILENO)) {
-		std::cerr << "output is not a terminal" << std::endl;
-		return 0;
-	}
-	if (!isatty(STDIN_FILENO)) {
-		std::cerr << "input is not a terminal" << std::endl;
-		return 0;
-	}
+    if (!isatty(STDOUT_FILENO)) {
+        std::cerr << "output is not a terminal" << std::endl;
+        return 0;
+    }
+    if (!isatty(STDIN_FILENO)) {
+        std::cerr << "input is not a terminal" << std::endl;
+        return 0;
+    }
     Terminal::init();
     char c;
-	ui::Base::clear();
-	ui::Base::goTo(1, 1);
-	ui::Base::present();
+    ui::Base::clear();
+    ui::Base::goTo(1, 1);
+    ui::Base::present();
     while (true) {
         if (read(STDIN_FILENO, &c, 1) > 0) {
-			if (c == 'q') {
-				break;
-			}
-			std::string s;
-			s += c;
-			s += "\r\n";
-			ui::Base::print(s);
-			ui::Base::present();
-		}
+            if (c == 'q') {
+                break;
+            }
+            std::string s;
+            s += c;
+            s += "\r\n";
+            ui::Base::print(s);
+            ui::Base::present();
+        }
     }
     return 0;
 }
--- src/terminal.cpp
+++ src/terminal.cpp
@@ -6,24 +6,24 @@
 static termios g_originalTermios;
 
 Terminal::Terminal() {
-	tcgetattr(STDOUT_FILENO, &g_originalTermios);
+    write(STDOUT_FILENO, "\x1b[?1049h", sizeof("\x1b[?1049h"));
+    tcgetattr(STDOUT_FILENO, &g_originalTermios);
 
-	termios term = g_originalTermios;
-	
-	term.c_iflag &= ~(IXON | IXOFF | ICRNL | IMAXBEL | BRKINT | IGNBRK);
-	term.c_oflag &= ~(OPOST);
-	term.c_cflag |= CS8;
-	term.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-	term.c_cc[VMIN] = 0;
-	term.c_cc[VTIME] = 1;
+    termios term = g_originalTermios;
 
-	tcsetattr(STDOUT_FILENO, TCSAFLUSH, &term);
+    term.c_iflag &= ~(IXON | IXOFF | ICRNL | IMAXBEL | BRKINT | IGNBRK);
+    term.c_oflag &= ~(OPOST);
+    term.c_cflag |= CS8;
+    term.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    term.c_cc[VMIN] = 0;
+    term.c_cc[VTIME] = 1;
+
+    tcsetattr(STDOUT_FILENO, TCSAFLUSH, &term);
 }
 
 Terminal::~Terminal() {
-	tcsetattr(STDOUT_FILENO, TCSAFLUSH, &g_originalTermios);
+    write(STDOUT_FILENO, "\x1b[?1049l", sizeof("\x1b[?1049l"));
+    tcsetattr(STDOUT_FILENO, TCSAFLUSH, &g_originalTermios);
 }
 
-void Terminal::init() {
-	static Terminal instance;
-}
+void Terminal::init() { static Terminal instance; }
