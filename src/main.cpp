63b1da5e20bdd0f5b45fb7299295d2c81c1ec4f5
mdco-ld <migueldco14@gmail.com>
2025-02-04 19:36:30 -0300

    detect if input and output are terminals

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,17 @@
 
 #include <unistd.h>
 
+#include <iostream>
+
 int main() {
+	if (!isatty(STDOUT_FILENO)) {
+		std::cerr << "output is not a terminal" << std::endl;
+		return 0;
+	}
+	if (!isatty(STDIN_FILENO)) {
+		std::cerr << "input is not a terminal" << std::endl;
+		return 0;
+	}
     Terminal::init();
     char c;
 	ui::Base::clear();
